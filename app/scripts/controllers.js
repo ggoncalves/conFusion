47cbb7d076f3df27a751b8e8937c51c9da284b61
ggoncalves <glaucio.c.goncalves@gmail.com>
2015-12-25 17:20:14 -0300

    Implementation of week3 Exercise: Angular ngRoute and SPAs.
    - Installing ngRoute using bower;
    - Change ng-include to ng-view;
    - Add route providers in app.js;
    - Changes in controller to retrieve a dish by id using routeParams.

--- app/scripts/controllers.js
+++ app/scripts/controllers.js
@@ -81,9 +81,11 @@ angular.module('confusionApp')
     };
 }])
 
-.controller('DishDetailController', ['$scope', 'menuFactory', function($scope, menuFactory) {
+.controller('DishDetailController', ['$scope', 'menuFactory', '$routeParams', function($scope, menuFactory, $routeParams) {
 
-    $scope.dish= menuFactory.getDish(3);
+    // $scope.dish= menuFactory.getDish(3);
+    var dish = menuFactory.getDish(parseInt($routeParams.id, 10));
+    $scope.dish = dish;
 
 }])
 
